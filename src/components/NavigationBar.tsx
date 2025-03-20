6fc543bc85a062cd368de8e4e6c4eae379725cc4
parksangryong <parksangryong@Mac-mini-2.local>
2025-03-20 18:00:17 +0900

    작업중

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,51 @@
+import IconButton from "./IconButton";
+
+interface NavigationBarProps {
+  isDark: boolean;
+  showBackButton: boolean;
+  showCloseButton: boolean;
+  showTitle: boolean;
+  title?: string;
+  onBackButtonClick?: React.MouseEventHandler<HTMLButtonElement>;
+  onCloseButtonClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const NavigationBar = ({
+  isDark,
+  showBackButton,
+  showCloseButton,
+  showTitle,
+  title = "",
+  onBackButtonClick = () => {},
+  onCloseButtonClick = () => {},
+}: NavigationBarProps) => {
+  return (
+    <div className="w-full flex justify-between items-center">
+      <div className="flex items-center navigation-title-wrapper">
+        {showBackButton && (
+          <IconButton
+            alt="back-arrow"
+            iconPath={
+              isDark
+                ? "/ic-public-back-arrow-white.svg"
+                : "/ic-public-back-arrow-black.svg"
+            }
+            onClick={onBackButtonClick}
+          />
+        )}
+        {showTitle && <h1 className="text-2xl !font-bold">{title}</h1>}
+      </div>
+      {showCloseButton && (
+        <IconButton
+          alt="close"
+          iconPath={
+            isDark ? "/ic-public-close-white.svg" : "/ic-public-close-black.svg"
+          }
+          onClick={onCloseButtonClick}
+        />
+      )}
+    </div>
+  );
+};
+
+export default NavigationBar;
---
+++ src/stories/NavigationBar.stories.tsx
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import NavigationBar from "../components/NavigationBar";
+
+const meta = {
+  title: "UI/NavigationBar",
+  component: NavigationBar,
+  decorators: [
+    (Story) => (
+      <div style={{ width: "360px" }}>
+        <Story />
+      </div>
+    ),
+  ],
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    isDark: { control: "boolean", description: "다크 모드 여부" },
+    showBackButton: {
+      control: "boolean",
+      description: "뒤로가기 버튼 표시 여부",
+    },
+    showCloseButton: { control: "boolean", description: "닫기 버튼 표시 여부" },
+    showTitle: { control: "boolean", description: "제목 표시 여부" },
+    title: { control: "text", description: "제목" },
+    onBackButtonClick: {
+      action: "backButtonClick",
+      description: "뒤로가기 버튼 클릭 이벤트",
+    },
+    onCloseButtonClick: {
+      action: "closeButtonClick",
+      description: "닫기 버튼 클릭 이벤트",
+    },
+  },
+  args: {
+    onBackButtonClick: fn(),
+    onCloseButtonClick: fn(),
+  },
+} satisfies Meta<typeof NavigationBar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    showBackButton: true,
+    showCloseButton: true,
+    showTitle: true,
+    title: "NavigationBar",
+    onBackButtonClick: fn(),
+    onCloseButtonClick: fn(),
+  },
+};
